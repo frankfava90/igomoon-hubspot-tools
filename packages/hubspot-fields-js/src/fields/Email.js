c289a3f3a8a9f7e9275a6ec74fcfc54ae78ad296
Frank Fava <frank.fava@gmail.com>
2022-10-23 00:50:20 +0200

    Email Field

--- packages/hubspot-fields-js/src/fields/Email.js
+++ packages/hubspot-fields-js/src/fields/Email.js
@@ -1,4 +1,60 @@
 const _HublField = require('./_HublField');
 
+/**
+ * @typedef {Array<string>|null} EmailDefault 
+ * 
+ * @typedef {Object} EmailDefinition
+ * @property {EmailDefault} default Array of email address strings.
+ * 
+ * @typedef {_HublField.FieldType & EmailDefinition} EmailType
+ * Combine into complete definition for Date Time
+ */
+
+/**
+ * Email Field Class
+ * @since 2.0.0
+ */
 module.exports = class Email extends _HublField {
+
+	/**
+	 * Email Field Constructor
+	 * 
+	 * Email address fields allow a user to select multiple email addresses. This could be used for displaying contact information. The email field returns an array of selected email addresses you can loop through. 
+	 * 
+	 * Email fields are supported in modules.
+	 * 
+	 * @see {@link https://developers.hubspot.com/en/docs/cms/building-blocks/module-theme-fields#email-address}
+	 * @param {EmailType} data
+	 */
+	constructor(data) {
+		super(data);
+
+		this.data = Object.assign(this.data, {
+			/**
+			 * @readonly
+			 * @private
+			 */
+			type: "email",
+			
+			// Name and Label
+			name: this.data.name || 'email_field',
+			label: this.data.label || 'Email Address field'
+		});
+
+		// Default
+		this.default(this.data.default)
+	}
+
+	/**
+	 * Set Default
+	 * @param {EmailDefault} emails Array of email address strings.
+	 */
+	default(emails) {
+		emails = Array.isArray(emails) ? emails : []
+		emails = (emails || this.data.default || []) 
+			.filter(email => (!!email && new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'i').test(email)))
+		this.data.default = !!emails.length ? emails : null
+		return this
+	}
+
 }
